670c6015bcf1fac0046b61b6bda03fbf33276beb
turqosoft <turqosoft@gmail.com>
2024-11-03 23:15:43 +0530

    Changed Logo

--- frontend/src/components/Icons/CRMLogo.vue
+++ frontend/src/components/Icons/CRMLogo.vue
@@ -12,10 +12,9 @@
     />
 
     <path d="M 213.571 0.745 L 84.999 0.745 C 37.66 0.745 -0.715 39.121 -0.715 86.459 L -0.715 215.031 C -0.715 262.369 37.66 300.745 84.999 300.745 L 213.571 300.745 C 260.909 300.745 299.285 262.369 299.285 215.031 L 299.285 86.459 C 299.285 39.121 260.909 0.745 213.571 0.745 Z" style="fill: rgb(5, 5, 5);"/>
-    <text style="white-space: pre; fill: rgb(64, 224, 208); font-family: Arial, sans-serif; font-size: 250px;" x="26.256" y="241.03">t</text>
-    <text style="white-space: pre; fill: rgb(255, 255, 255); font-family: Arial, sans-serif; font-size: 255px;" x="89.148" y="231.668" transform="matrix(1.062127, 0, 0, 1.031312, -14.968895, 5.505346)">Q</text>
-
-    <ellipse style="stroke: rgb(0, 0, 0); fill: rgb(64, 224, 208);" cx="279.243" cy="218.925" rx="15.486" ry="15.486"/>
+    <text style="white-space: pre; fill: rgb(64, 224, 208); font-family: Arial, sans-serif; font-size: 250px;" x="12.256" y="241.03">t</text>
+    <text style="white-space: pre; fill: rgb(255, 255, 255); font-family: Arial, sans-serif; font-size: 255px;" x="89.148" y="231.668" transform="matrix(1.130569, 0, 0, 1.048759, -34.070374, 0.518483)">Q</text>
+    <ellipse style="stroke: rgb(0, 0, 0); fill: rgb(64, 224, 208);" cx="278.074" cy="216.831" rx="15.486" ry="15.486"/>
     
   </svg>
   
